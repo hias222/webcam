61afc039ca919cec5a6138b93ad439d9bf2488d3
matthias <matthias.fuchs>
2021-01-09 21:14:09 +0100

    resolution

--- client/src/routes/Watch.js
+++ client/src/routes/Watch.js
@@ -4,17 +4,13 @@ import Peer from "simple-peer";
 import styled from "styled-components";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    height: 100vh;
-    width: 90%;
-    margin: auto;
-    flex-wrap: wrap;
+    padding: 0px;
+    margin: 0px;
 `;
 
 const StyledVideo = styled.video`
     height: 100%;
-    width: 100%;
+    width: 1280px;
 `;
 
 const Video = (props) => {
